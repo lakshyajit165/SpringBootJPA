9758d20745a7ebcce89b922d69cef7d213f0090a
lakshyajit <lakshyajit165@gmail.com>
2019-09-02 20:03:11 +0530

    Added get request to get details, using a particular ID

--- src/main/java/com/stackroute/SpringBootJPA/controller/AlienController.java
+++ src/main/java/com/stackroute/SpringBootJPA/controller/AlienController.java
@@ -5,6 +5,8 @@ import com.stackroute.SpringBootJPA.model.Alien;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AlienController {
@@ -21,5 +23,13 @@ public class AlienController {
     public String addAlien(Alien alien){
         repo.save(alien);
         return "home.jsp";
+    }
+
+    @RequestMapping("/getAlien")
+    public ModelAndView addAlien(@RequestParam int aid){
+        ModelAndView mv = new ModelAndView("showAlien.jsp");
+        Alien alien = repo.findById(aid).orElse(new Alien());
+        mv.addObject(alien);
+        return mv;
     }
 }
