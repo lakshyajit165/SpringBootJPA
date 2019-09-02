72d445eb473f7fdf5ceb000625eaa172c02858b0
lakshyajit <lakshyajit165@gmail.com>
2019-09-02 22:39:40 +0530

    Using route parameters

--- src/main/java/com/stackroute/SpringBootJPA/controller/AlienController.java
+++ src/main/java/com/stackroute/SpringBootJPA/controller/AlienController.java
@@ -4,8 +4,10 @@ import com.stackroute.SpringBootJPA.dao.AlienRepo;
 import com.stackroute.SpringBootJPA.model.Alien;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -25,18 +27,18 @@ public class AlienController {
         return "home.jsp";
     }
 
-    @RequestMapping("/getAlien")
-    public ModelAndView addAlien(@RequestParam int aid){
+    @RequestMapping("/aliens")
+    @ResponseBody
+    public String getAliens(){
 
-        ModelAndView mv = new ModelAndView("showAlien.jsp");
-        Alien alien = repo.findById(aid).orElse(new Alien());
+        return repo.findAll().toString();
+    }
 
-        System.out.println(repo.findByTech("Java"));
-        System.out.println(repo.findByAidGreaterThan(102));
-        System.out.println(repo.findByTechSorted("Java"));
+    @RequestMapping("/alien/{aid}")
+    @ResponseBody
+    public String getAlien(@PathVariable("aid") int aid){
 
-        mv.addObject(alien);
-        return mv;
-
+        return repo.findById(aid).toString();
     }
 }
+
