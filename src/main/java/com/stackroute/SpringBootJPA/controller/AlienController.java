f5dd8239e8e44a0e6c41049b87eeaa15ee5e0076
Lakshyajit <lakshyajit165@gmail.com>
2019-09-03 10:31:34 +0530

    Sending a post request from java

--- src/main/java/com/stackroute/SpringBootJPA/controller/AlienController.java
+++ src/main/java/com/stackroute/SpringBootJPA/controller/AlienController.java
@@ -4,16 +4,13 @@ import com.stackroute.SpringBootJPA.dao.AlienRepo;
 import com.stackroute.SpringBootJPA.model.Alien;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class AlienController {
 
     @Autowired
@@ -24,21 +21,28 @@ public class AlienController {
         return "home.jsp";
     }
 
-    @RequestMapping("/addAlien")
-    public String addAlien(Alien alien){
+//    @RequestMapping("/addAlien")
+//    public String addAlien(Alien alien){
+//        repo.save(alien);
+//        return "home.jsp";
+//    }
+
+    @PostMapping(path="/alien", consumes={"application/json"})
+    public Alien addAlien(@RequestBody Alien alien){
         repo.save(alien);
-        return "home.jsp";
+        return alien;
     }
 
-    @RequestMapping(path="/aliens", produces = {"application/xml"})
-    @ResponseBody
+
+    @GetMapping(path="/aliens")
+
     public List<Alien> getAliens(){
 
         return repo.findAll();
     }
 
     @RequestMapping("/alien/{aid}")
-    @ResponseBody
+
     public Optional<Alien> getAlien(@PathVariable("aid") int aid){
 
         return repo.findById(aid);
